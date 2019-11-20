15d360e488042eb562828ac74cc3ea3ebac4a15d
Fabian Wunsch <fabian@uriah.heep.sax.de>
2019-11-20 22:43:23 +0100

    Bugfix

--- Classes/NameTripResponse.py
+++ Classes/NameTripResponse.py
@@ -5,5 +5,5 @@ from Classes.Stop import StopWithoutLine
 class NameStopResponse(IDTripResponse):
     def __init__(self, start: str, end: str, request_time: int, **kwargs):
         self._start_stop: StopWithoutLine = IDStopNameResponse(start).get_stop()
-        self._end_stop: StopWithoutLine = IDStopNameResponse(start).get_stop()
+        self._end_stop: StopWithoutLine = IDStopNameResponse(end).get_stop()
         IDTripResponse.__init__(self, self._start_stop, self._end_stop, request_time, **kwargs)

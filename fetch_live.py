4604d4708768581d10832b32805cb093db3846ef
Fabian Wunsch <fabian@uriah.heep.sax.de>
2020-01-16 22:16:08 +0100

    repaired fetch_live, improved structure of main

--- fetch_live.py
+++ fetch_live.py
@@ -1,37 +1,50 @@
+import logging
 import time
 from multiprocessing import Pool
 from typing import List, Any, Tuple, Dict
 
-from Classes.BaseTypes.Journey import Journey
+import ZODB.config
+from ZODB.Connection import Connection
+
+from Classes.DBManager.DBJourney import DBJourney
+from Classes.DBManager.DBLine import DBLine
+from Classes.DBManager.DBSection import DBSection
+from Classes.DBManager.DBStop import DBStop
 from Classes.HighRequests.IDStopResponse import IDStopResponse
-from Classes.MergeableList import MergeableList
-import logging
+from Classes.Utilities.db_config import zodb_conf
+from Classes.Utilities.typings import UnixTime, StopWithoutLineStr, JourneyStr
 
 logger: logging.Logger = logging.getLogger('fetch_live')
 
+db: ZODB.DB = ZODB.config.databaseFromString(zodb_conf)
 
-def worker(args_kwargs: Tuple[Any, int, Dict[str, Any]]):
+
+def worker(args_kwargs: Tuple[StopWithoutLineStr, UnixTime, Dict[str, Any]]):
     return IDStopResponse(args_kwargs[0], args_kwargs[1], **args_kwargs[2])
 
 
 def fetch_live():
-    start_time: int = time.time()
+    connection: Connection = db.open()
+    start_time: UnixTime = UnixTime(int(time.time()))
     logger.info('Fetching live data')
+    db_stops: DBStop = connection.root().stops
     args: List[Tuple[Any, int, Dict[str, Any]]] = []
-    for i in init_data[1]:
+    for i in db_stops:
         args.append((i, int(time.time()), {'calculate_lines': True}))
     with Pool(20) as p:
-        output: MergeableList[IDStopResponse] = MergeableList(p.map(worker, args))
+        output: List[IDStopResponse] = p.map(worker, args)
     logger.info('Getting journeys from live data')
-    journeys: List[List[Journey]] = []
+    journeys: List[List[JourneyStr]] = []
+    db_sections: DBSection = connection.root().sections
+    db_lines: DBLine = connection.root().lines
+    db_journeys: DBJourney = connection.root().journeys
     for i in output:
-        journeys.append(i.get_journeys(init_data[2]))
+        journeys.append(i.get_journeys(db_sections.keys()))
     logger.info('Merging Journeys with existing data')
     for i in journeys:
         if len(i) >= 1:
             for i in i:
-                for j in init_data[0]:
-                    if str(j) == str(i.get_line()):
+                for j in db_lines.values():
+                    if str(j) == str(db_journeys[i].get_line()):
                         j.add_journey(i)
     logger.info('Fetched live data in ' + str(time.time() - start_time) + ' seconds.')
-    return init_data
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
+import time
 
 from fetch_init import fetch_init
-# from fetch_live import fetch_live
+from fetch_live import fetch_live
 from generate_json import generate_json
 
 logging.basicConfig(level=logging.WARNING, format='[%(levelname)s] %(asctime)s %(name)s [%(funcName)s]: %(message)s',
@@ -9,16 +10,11 @@ logging.basicConfig(level=logging.WARNING, format='[%(levelname)s] %(asctime)s %
 
 debug: bool = True
 fetch_init(debug)
-abschnitte, haltestellen = generate_json()
-with open('frontend/abschnitte.json', 'w') as f:
-    f.write(abschnitte)
-with open('frontend/haltestelle.json', 'w') as f:
-    f.write(haltestellen)
-# while True:
-#    fetch_live()
-#    abschnitte, haltestellen = generate_json()
-#    with open('frontend/abschnitte.json', 'w') as f:
-#        f.write(abschnitte)
-#    with open('frontend/haltestelle.json', 'w') as f:
-#        f.write(haltestellen)
-#    time.sleep(30)
+while True:
+    abschnitte, haltestellen = generate_json()
+    with open('frontend/abschnitte.json', 'w') as f:
+        f.write(abschnitte)
+    with open('frontend/haltestelle.json', 'w') as f:
+        f.write(haltestellen)
+    time.sleep(30)
+    fetch_live()
